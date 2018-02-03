cb0032972a3872972fef48f2ce0239529bf5bbe0
OCclan <33261397+OCclan@users.noreply.github.com>
2018-02-04 03:08:01 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const Discord = require("Discord.js");
+const Discord = require("discord.js");
+
+const TOKEN = "NDA5NDY1MzQ5ODM5MjU3NjAw.DVe_1w.4gZtWQ4bHOH5MXmb7OryRHhFDU0";
 
 var bot = new Discord.Client();
 
 bot.on("message", function(message) {
-    console.log(message.content);
+    console.log("Ready");
+});
+
+bot.on("message", function(message) {
+    if(message.author.equals(bot.user)) return;
+
+    if(message.content == "hello") {
+        message.channel.sendMessage("Hi, there!");
+    }
 });
 
 bot.login(process.env.BOT_TOKEN);
